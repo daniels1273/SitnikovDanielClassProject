923e11bc8fc39e4a4d3f0401dd85e87312c25f96
asjkdhkajsh <skjdhksjdhf@kjsdhf>
2026-01-28 11:04:00 -0500

    added javadoc comments for the Player class

--- src/Player.java
+++ src/Player.java
@@ -1,36 +1,69 @@
+/**
+ * This class represents a player object
+ *
+ * @author Daniel Sitnikov
+ */
 public class Player {
 
+    /** The player's current health */
     private int health;
+
+    /** The player's max health */
     private final int MAXHP;
+
+    /** The amount of times the player has revived */
     private int revives;
 
+    /** Instantiates a Player object */
     public Player(){
         health = 25;
         MAXHP = health;
         revives = 0;
     }
 
+    /**
+     * Returns the player's current health
+     *
+     * @return The player's current health
+     */
     public int getHealth(){
         return health;
     }
 
+    /**
+     * Returns the amount of times the player has successfully been revived
+     *
+     * @return The amount of times the player was revived
+     */
     public int getRevives(){
         return revives;
     }
 
+    /**
+     * Restores the player's current health to their max health
+     */
     public void restoreHealth(){
         health = MAXHP + 1;
     }
 
+    /**
+     * Decrements the player's current health by one
+     */
     public void hurt(){
         health--;
     }
 
+    /**
+     * Kills the player by decreasing their health to 1, also runs the secondChance method
+     */
     public void die(){
         health = 1;
         secondChance();
     }
 
+    /**
+     * Has a chance of reviving the player
+     */
     //gives the player possibly a second chance to keep going
     //functions off of dnd's death saving throws mechanic
     //rolls a 20-sided dice, a 20 instant succeeds (revives player), a 1 is two failures,
